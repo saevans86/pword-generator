4cf8f50c25d9617ac82d387f4d71d66907b59a7f
saevans86 <sam_evans1986@hotmail.com>
2023-09-23 11:08:00 -0600

    changed html not displaying correctly, added generate function for the checkboxes, need randomizer

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ var symbolsEl = document.getElementById('symbols');
 
 
 // Write password to the #password input
+function generatePassword() {
+  var length = +lengthEl.value;
+  var hasLower = lowercaseEl.checked;
+  var hasUpper = uppercaseEl.checked;
+  var hasNumber = numbersEl.checked;
+  var hasSymbol = symbolsEl.checked;
+  //function to pull  pass deets and compare to whether the checkbox is used
+
+  let generatedPassword = '';
+  const typesCount = hasLower + hasUpper + hasNumber + hasSymbol;
+  
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
