df67c02f3a8e11dfd73f107b5dc9b37c85c9e9bb
saevans86 <sam_evans1986@hotmail.com>
2023-09-23 13:06:06 -0600

    added readme, pending live link, also added if that if nothing was checked nothing would be returned to prevent it from crashing upon click

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Password Generator Starter Code
+
+# Title 
+pword-generator
+
+
+
+
+## Description 
+This password generator serves a lot of uses since I am on my computer fairly often, and changing passwords occurs fairly regularly. This was previous done by following a guide, so I wanted to re-write the code. 
+
+
+
+
+
+## Table of Contents (Optional)
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Installation 
+clone repo, open the HTML as live server, or in default browser, or just click this link for the live version ()
+
+
+
+## Usage 
+You can choose how many characters your password should be, and to include any numbers, symbols, lower/upper case and just click the generate password button.
+
+
+
+    
+
+## Credits 
+The base code can be seen here: https://github.com/coding-boot-camp/friendly-parakeet would also like to mention this post: https://stackoverflow.com/questions/61820662/how-to-filter-array-of-js-objects-by-an-array-of-boolean-values
+
+
+
+## Features 
+It allows the user to create a password of random characters, up to 128 characters in length.
+
+
+
+
+
+
+
+## Tests 
+No test ran.
+
+
+
+## Questions 
+# Please contact me at:
+ github username: saevans86 e-mail: Please contact me on github
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment code here
 var passDeets = {
-  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  lowerCase: 'abcdefghijklmnopqrstuvwxyz',
-  symbols: '!@#$%^&*(){}[]=<>/,.',
-  numbers: '0123456789',
+	upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+	symbols: '!@#$%^&*(){}[]=<>/,.',
+	numbers: '0123456789'
 };
 
 function randomizer(passdeetsArray) {
-  const randomIndex = Math.floor(Math.random() * passdeetsArray.length);
-  return passdeetsArray[randomIndex];
+	const randomIndex = Math.floor(Math.random() * passdeetsArray.length);
+	return passdeetsArray[randomIndex];
 }
 
 // Get references to the #generate element
@@ -19,46 +19,45 @@ var lowercaseEl = document.getElementById('lowercase');
 var numbersEl = document.getElementById('numbers');
 var symbolsEl = document.getElementById('symbols');
 
-
 // Write password to the #password input
 function generatePassword() {
-  var length = +lengthEl.value;
-  var hasLower = lowercaseEl.checked;
-  var hasUpper = uppercaseEl.checked;
-  var hasNumber = numbersEl.checked;
-  var hasSymbol = symbolsEl.checked;
-  //function to pull  pass deets and compare to whether the checkbox is used
+	var length = +lengthEl.value;
+	var hasLower = lowercaseEl.checked;
+	var hasUpper = uppercaseEl.checked;
+	var hasNumber = numbersEl.checked;
+	var hasSymbol = symbolsEl.checked;
+	//function to pull  pass deets and compare to whether the checkbox is used
 
-  let generatedPassword = '';
+	let generatedPassword = '';
   const typesCount = hasLower + hasUpper + hasNumber + hasSymbol;
-
-  const passdeetsArray = [
-    hasLower && passDeets.lowerCase,
-    hasUpper && passDeets.upperCase,
-    hasNumber && passDeets.numbers,
-    hasSymbol && passDeets.symbols,
-  ].filter(Boolean);
-
-  for (let i = 0; i < length; i += typesCount) {
-    passdeetsArray.forEach((type) => {
-      if (generatedPassword.length < length) {
-        generatedPassword += randomizer(type);
-      }
-    });
+  if (typesCount === 0) {
+    return '';
   }
 
-  return generatedPassword;
+	const passdeetsArray = [
+		hasLower && passDeets.lowerCase,
+		hasUpper && passDeets.upperCase,
+		hasNumber && passDeets.numbers,
+		hasSymbol && passDeets.symbols
+	].filter(Boolean);
+
+	for (let i = 0; i < length; i += typesCount) {
+		passdeetsArray.forEach((type) => {
+			if (generatedPassword.length < length) {
+				generatedPassword += randomizer(type);
+			}
+		});
+	}
+
+	return generatedPassword;
 }
 
-
-
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var password = generatePassword();
+	var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
